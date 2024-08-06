46f2ff83e29a11fe2408a3683084a68800b3ecbd
Rasmus Tanggaard <rasmus@tthost.dk>
2024-08-06 14:20:07 +0200

    Added first iteration of SPA

---
+++ VisualizeApiData/Services/CircleK.cs
@@ -0,0 +1,34 @@
+﻿using System.Net.Http.Json;
+
+namespace VisualizeApiData.Services
+{
+    public class CircleK
+    {
+        private readonly HttpClient _httpClient;
+
+        public CircleK(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public string GetBaseURL()
+        {
+            return _httpClient.BaseAddress.ToString();
+        }
+
+        public async Task<T?> GetAsync<T>(string requestUri)
+        {
+            try
+            {
+                var response = await _httpClient.GetAsync(requestUri);
+                response.EnsureSuccessStatusCode();
+                return await response.Content.ReadFromJsonAsync<T>();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return default;
+            }
+        }
+    }
+}
